c6fd39e1f7608c3492474182f0c04e8d91ab466f
Tyler Hancock <darklime@live.ca>
2022-03-17 21:36:17 -0600

    Port to 1.18.2

--- Common/src/main/java/net/darkhax/noaispawneggs/Constants.java
+++ Common/src/main/java/net/darkhax/noaispawneggs/Constants.java
@@ -1,11 +1,7 @@
 package net.darkhax.noaispawneggs;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class Constants {
 
     public static final String MOD_ID = "noaispawneggs";
     public static final String MOD_NAME = "No AI Spawn Eggs";
-    public static final Logger LOG = LogManager.getLogger(MOD_NAME);
 }
